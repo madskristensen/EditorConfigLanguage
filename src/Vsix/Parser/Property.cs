50a682f209885533f94dff0153cb5b087cca0459
Mads Kristensen <post@madskristensen.net>
2016-12-08 08:47:18 -0800

    Added "false" value to "root" property

--- src/Vsix/Parser/Property.cs
+++ src/Vsix/Parser/Property.cs
@@ -11,7 +11,7 @@ namespace EditorConfig
         private static List<Property> _cache = new List<Property>
         {
             // Standard properties
-            {new Property("root", true, Resources.Text.KeywordRoot, KnownMonikers.Property, "true")},
+            {new Property("root", true, Resources.Text.KeywordRoot, KnownMonikers.Property, "true", "false")},
             {new Property("charset", true, Resources.Text.KeywordCharset, KnownMonikers.Property, "latin1", "utf-8", "utf-8-bom", "utf-16be", "utf-16le", "utf-8-bom")},
             {new Property("end_of_line", true, Resources.Text.KeywordEndOfLine, KnownMonikers.Property, "lf", "crlf", "cr") },
             {new Property("indent_style", true, Resources.Text.KeywordIndentStyle, KnownMonikers.Property, "tab", "space")},

95682c723d72832d96a97a0ea91c28f100854e35
Mads Kristensen <post@madskristensen.net>
2017-01-31 13:27:29 -0800

    Created infrastructure to test parser and validator

---
+++ Test/EditorConfigTest/DocumentTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Reflection;
+
+namespace EditorConfigTest
+{
+    [TestClass]
+    public class DocumentTest
+    {
+        [Import]
+        private ITextBufferFactoryService TextBufferService { get; set; }
+
+        [Import]
+        private IContentTypeRegistryService ContentTypeService { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var folder = new DirectoryInfo(@"..\..\..\..\lib\").FullName;
+            var editor = Assembly.LoadFrom(folder + "Microsoft.VisualStudio.Platform.VSEditor.dll");
+            var interop = Assembly.LoadFrom(folder + "Microsoft.VisualStudio.Platform.VSEditor.interop.dll");
+            var text = Assembly.LoadFrom(folder + "Microsoft.VisualStudio.Text.Internal.dll");
+
+            AggregateCatalog catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(editor));
+            catalog.Catalogs.Add(new AssemblyCatalog(interop));
+            catalog.Catalogs.Add(new AssemblyCatalog(text));
+
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(DocumentTest).Assembly));
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(EditorConfig.EditorConfigDocument).Assembly));
+
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.SatisfyImportsOnce(this);
+        }
+
+        [TestMethod]
+        public void GetHighlightedSpans()
+        {
+            var textbuffer = Mef.CreateTextbuffer("test"); //TextBufferService.CreateTextBuffer("test", ContentTypeService.UnknownContentType);
+
+            Assert.AreEqual("test", textbuffer.CurrentSnapshot.GetText());
+
+            Assert.IsNotNull(TextBufferService);
+            Assert.IsNotNull(ContentTypeService);
+        }
+    }
+}
---
+++ Test/EditorConfigTest/Helpers/Mef.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Reflection;
+
+namespace EditorConfigTest
+{
+    public class Mef
+    {
+        [Import]
+        private ITextBufferFactoryService TextBufferService { get; set; }
+
+        [Import]
+        private IContentTypeRegistryService ContentTypeService { get; set; }
+
+        public Mef()
+        {
+            var folder = new DirectoryInfo(@"..\..\..\..\lib\").FullName;
+            var editor = Assembly.LoadFrom(folder + "Microsoft.VisualStudio.Platform.VSEditor.dll");
+            var interop = Assembly.LoadFrom(folder + "Microsoft.VisualStudio.Platform.VSEditor.interop.dll");
+            var text = Assembly.LoadFrom(folder + "Microsoft.VisualStudio.Text.Internal.dll");
+
+            AggregateCatalog catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(editor));
+            catalog.Catalogs.Add(new AssemblyCatalog(interop));
+            catalog.Catalogs.Add(new AssemblyCatalog(text));
+
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(DocumentTest).Assembly));
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(EditorConfig.EditorConfigDocument).Assembly));
+
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.SatisfyImportsOnce(this);
+        }
+
+        public static ITextBuffer CreateTextbuffer(string text)
+        {
+            var mef =  new Mef();
+            return mef.TextBufferService.CreateTextBuffer(text, mef.ContentTypeService.UnknownContentType);
+        }
+    }
+}
